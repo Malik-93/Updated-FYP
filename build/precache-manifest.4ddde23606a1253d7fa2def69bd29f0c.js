156b5884e349ac4087bd5c04dabdc0eee7b1874c
M Mudassir <Malikjrw147@yahoo.com>
2019-05-11 16:18:38 -0700

    removed build folder from  git ignore

---
+++ build/precache-manifest.4ddde23606a1253d7fa2def69bd29f0c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "84950295113765708dd3",
+    "url": "/static/css/main.4c3ad7c4.chunk.css"
+  },
+  {
+    "revision": "84950295113765708dd3",
+    "url": "/static/js/main.7564e65d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "039acac47bcde134a6c7",
+    "url": "/static/css/2.f4982b5b.chunk.css"
+  },
+  {
+    "revision": "039acac47bcde134a6c7",
+    "url": "/static/js/2.65a7e7ab.chunk.js"
+  },
+  {
+    "revision": "9b5bfe753bffac94a9aaed9de6951476",
+    "url": "/static/media/men.9b5bfe75.jpg"
+  },
+  {
+    "revision": "cfb1476119ad81e8ffc7481df6efffd0",
+    "url": "/static/media/men-2.cfb14761.jpg"
+  },
+  {
+    "revision": "521ef10fe105ff6618879868151083e1",
+    "url": "/static/media/men-3.521ef10f.jpg"
+  },
+  {
+    "revision": "56a7abf22a7756c6d94c03e2c2df64f2",
+    "url": "/static/media/men-4.56a7abf2.jpg"
+  },
+  {
+    "revision": "7dbc0aa1d875a90c442f75a1b374932f",
+    "url": "/static/media/men-5.7dbc0aa1.jpg"
+  },
+  {
+    "revision": "5b75309b8a2142d01a5e1babdad23513",
+    "url": "/static/media/womens.5b75309b.jpg"
+  },
+  {
+    "revision": "c0d9fd1eb99e10ed6e8e07c0461f642c",
+    "url": "/static/media/women-2.c0d9fd1e.jpg"
+  },
+  {
+    "revision": "73d5516f12930916e63afe18c23edaeb",
+    "url": "/static/media/women-3.73d5516f.jpeg"
+  },
+  {
+    "revision": "f92335de40ac687fe5b39ae56dcf5bf8",
+    "url": "/static/media/women-4.f92335de.jpg"
+  },
+  {
+    "revision": "e0f2cc380e56499edaf3a9ffd4b029e5",
+    "url": "/static/media/women-5.e0f2cc38.jpg"
+  },
+  {
+    "revision": "8b14016f2ee9fa855c10d84e74dd7aa9",
+    "url": "/static/media/kids.8b14016f.jpg"
+  },
+  {
+    "revision": "11e9b4fb4bea058740f90224ebfb4108",
+    "url": "/static/media/kid-2.11e9b4fb.jpg"
+  },
+  {
+    "revision": "ec5f0500886b75d36669d742b100b6ee",
+    "url": "/static/media/kid-3.ec5f0500.jpg"
+  },
+  {
+    "revision": "5cdde08067159c7a405c58d5d999d905",
+    "url": "/static/media/kid-4.5cdde080.jpg"
+  },
+  {
+    "revision": "ebc9b4658ae3c5a317e4f1a02008743b",
+    "url": "/static/media/kid-5.ebc9b465.jpg"
+  },
+  {
+    "revision": "7cc7a630624d20f7797cb4c8e93c09c1",
+    "url": "/static/media/user.7cc7a630.png"
+  },
+  {
+    "revision": "7614aa397523421f1fe01747d4136c79",
+    "url": "/static/media/img-1.7614aa39.jpg"
+  },
+  {
+    "revision": "f9df3a664ab26e3dbcc1314b19b55b56",
+    "url": "/static/media/img-2.f9df3a66.jpg"
+  },
+  {
+    "revision": "2a50bbe1aab3884f30c7c1b9e66500d3",
+    "url": "/static/media/img-3.2a50bbe1.jpg"
+  },
+  {
+    "revision": "24c05c482e2b0177a237c5ae1d020511",
+    "url": "/index.html"
+  }
+];
